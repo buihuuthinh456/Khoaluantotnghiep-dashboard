72de392750a02555467580712666a7e524901791
toanton <toanton0911@gmail.com>
2022-03-29 17:52:30 +0700

    ADD: sidebar update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import GlobalState from './GlobalState'
-import { Person, ShoppingCart, Login  } from '@material-ui/icons'
-
+import { Person, ShoppingCart, HowToReg } from '@material-ui/icons'
 
 function Sidebar() {
   return (
@@ -10,35 +9,36 @@ function Sidebar() {
         <GlobalState />
         <Logo>TShop</Logo>
 
-        <UserInfo>
-            <ImageWrap>
-                <img src='https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg' alt='user-ava'></img>
-            </ImageWrap>
-            <span>Toàn Tôn</span>
-        </UserInfo>
+        <Wrapper>
+            <UserInfo>
+                <ImageWrap>
+                    <img src='/images/default-user-image.jpg' alt='user-ava'></img>
+                </ImageWrap>
+                <span>Toàn Tôn</span>
+            </UserInfo>
+        </Wrapper>
 
         <SidebarList>
-            <SidebarItem>
+            <SidebarItem isSelected={true}>
                 <Person />
                 <span>User</span>
             </SidebarItem>
 
-            <SidebarItem>
+            <SidebarItem isSelected={false}>
                 <ShoppingCart />
                 <span>Product</span>
             </SidebarItem>
 
-            <SidebarItem>
-                <img src='/images/default-user-image.jpg' alt='login'></img>
-                <span>Dashboard</span>
+            <SidebarItem isSelected={false}>
+                <LoginIcon>
+                    <img src='images/outline_login_black_24dp.png' alt='login'></img>
+                </LoginIcon>
+                <span>Login</span>
             </SidebarItem>
 
-            <SidebarItem>
-                <span>Dashboard</span>
-            </SidebarItem>
-
-            <SidebarItem>
-                <span>Dashboard</span>
+            <SidebarItem isSelected={false}>
+                <HowToReg />
+                <span>Register</span>
             </SidebarItem>
         </SidebarList>
 
@@ -55,7 +55,6 @@ const Container = styled.div`
     left: 0;
     width: 275px;
     height: 100vh;
-    padding: 20px;
     z-index: 999;
     border-right: 1px solid #e1e5e9; 
 `
@@ -64,18 +63,25 @@ const Logo = styled.h1`
     font-size: 36px;
     font-weight: 600;
     color: var(--primary-color);
+    cursor: pointer;
+    padding: 20px;
+    margin: 0;
+`
+
+const Wrapper = styled.div`
+    padding: 20px;
+    height: 72px;
+    margin-bottom: 36px;
 `
 
 const UserInfo = styled.div`
     cursor: pointer;
     width: 100%;
-    height: 72px;
     padding: 16px 20px;
     background-color: #edeff2;
     border-radius: 10px;
     display: flex;
     align-items: center;
-    margin-bottom: 36px;
 
     span {
         font-size: 16px;
@@ -98,10 +104,42 @@ const ImageWrap = styled.div`
 `
 
 const SidebarList = styled.ul`
+    margin-top: 60px;
     padding-left: 0;
     list-style: none;
 `   
 
 const SidebarItem = styled.li`
+    cursor: pointer;
+    padding: 20px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
 
+    &:hover {
+        background-color: #f1f2f5;
+    }
+
+    ${state=>state.isSelected ? `
+        color: var(--primary-color);
+        border-right: 4px solid var(--primary-color);
+        background-color: #f1f2f5;   
+    `: ''};
+    span {
+        margin-left: 16px;
+        font-size: 14px;
+        font-weight: 400;
+    }
+`
+
+const LoginIcon = styled.div`
+    width: 24px;
+    height: 24px;
+
+    img {
+        width:100%;
+        height: 100%;
+        object-fit: cover;
+    }
 `
