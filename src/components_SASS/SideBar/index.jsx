4f946038034d29ac9fa85b5b7087a2d34ffe2314
toanton <toanton0911@gmail.com>
2022-05-17 14:09:33 +0700

    ADD: Sidebar SASS

--- src/components_SASS/SideBar/index.jsx
+++ src/components_SASS/SideBar/index.jsx
@@ -1,24 +1,67 @@
 import React from "react";
 import styles from "./Sidebar.module.scss";
 import SidebarItem from "../SidebarItem";
+
 import { Person, ShoppingCart, HowToReg } from "@material-ui/icons";
+import CategoryIcon from "@mui/icons-material/Category";
+import PlaylistAddCheckCircleIcon from "@mui/icons-material/PlaylistAddCheckCircle";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectLogin } from "../../features/login/loginSlice";
 
 function Sidebar() {
+  let location = useLocation();
+
+  const userInfo = useSelector(selectLogin).info;
+  const isLogin = useSelector(selectLogin).isLogin
+  // sidebar data
+  let sidebarItem = [
+    {
+      path: "/",
+      name: "User",
+      icon: "Person",
+    },
+    {
+      path: "/category",
+      name: "Category",
+      icon: "CategoryIcon",
+    },
+    {
+      path: "/products",
+      name: "Product",
+      icon: "PlaylistAddCheckCircleIcon",
+    },
+    {
+      path: "/orders",
+      name: "Orders",
+      icon: "ShoppingCart",
+    },
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.logo}>TShop</div>
 
-      <div className={styles.userInfo}>
-        <div className={styles.imgWrapper}>
-          <img src="/images/default-user-image.jpg" alt="user-ava" />
+      {isLogin && (
+        <div className={styles.userInfo}>
+          <div className={styles.imgWrapper}>
+            <img src="/images/default-user-image.jpg" alt="user-ava" />
+          </div>
+          <span>{userInfo.name}</span>
         </div>
-
-        <span>123456</span>
-      </div>
+      )}
 
       <ul className={styles.SidebarList}>
-        <SidebarItem isSelected={true} />
-        <SidebarItem isSelected={false} />
+        {sidebarItem.map((item, index) => {
+          return (
+            <SidebarItem
+              isSelected={location.pathname === item.path ? true : false}
+              key={index}
+              icon={item.icon}
+              name={item.name}
+              path={item.path}
+            />
+          );
+        })}
       </ul>
     </div>
   );
--- src/components_SASS/SidebarItem/index.jsx
+++ src/components_SASS/SidebarItem/index.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import styles from "./SidebarItem.module.scss";
 import { Person, ShoppingCart, HowToReg } from "@material-ui/icons";
+import CategoryIcon from "@mui/icons-material/Category";
+import PlaylistAddCheckCircleIcon from "@mui/icons-material/PlaylistAddCheckCircle";
+import { Link } from "react-router-dom";
 
-function SidebarItem({isSelected}) {
+function SidebarItem({ isSelected, icon, path, name }) {
   return (
-    <li className={`${styles.container} ${isSelected ? `${styles.active}` : ''}`}>
-      <div className={styles.icon}>
-        <Person style={{ fontSize: 24 }}></Person>
-      </div>
-      <span>123456</span>
-    </li>
+    <Link to={path} style={{ textDecoration: "none", color: "black" }}>
+      <li
+        className={`${styles.container} ${isSelected ? `${styles.active}` : ""}`}
+      >
+        <div className={styles.icon}>
+          {icon === "Person" && <Person style={{ fontSize: 24 }} />}
+          {icon === "CategoryIcon" && (
+            <CategoryIcon style={{ fontSize: 24 }} />
+          )}
+          {icon === "PlaylistAddCheckCircleIcon" && (
+            <PlaylistAddCheckCircleIcon style={{ fontSize: 24 }} />
+          )}
+          {icon === "ShoppingCart" && (
+            <ShoppingCart style={{ fontSize: 24 }} />
+          )}
+        </div>
+        <span>{name}</span>
+      </li>
+    </Link>
   );
 }
 
